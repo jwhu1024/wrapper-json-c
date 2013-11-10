fde6dc8e51b934baf0d677487792333341cf257a
lester_hu <jwhu1024@gmail.com>
2013-11-11 02:52:20 +0800

    1. Add some recursive function to getting value from object:  1) br_json_query_string  2) br_json_query_integer  3) br_json_query_boolean
    2. Use compile option to decide print or write format (_PRETTY_PRINT_)

--- include/jsonC.h
+++ include/jsonC.h
@@ -1,4 +1,12 @@
+#ifndef JSON_C_H
+#define JSON_C_H
 #define _DEBUG
+
+#ifdef _PRETTY_PRINT_
+#define JSON_FORMAT JSON_C_TO_STRING_PRETTY
+#else
+#define JSON_FORMAT JSON_C_TO_STRING_SPACED
+#endif
 
 #ifdef _DEBUG
 #define DEBUG(format, args...) printf("[%s:%d] "format, __func__, __LINE__, ##args)
@@ -26,6 +34,18 @@ void br_json_update_integer (json_object *obj, char *_key, int _value);
 void br_json_update_boolean (json_object *obj, char *_key, bool _value);
 
 /* Function for get value from object */
-char* br_json_get_string  (json_object *obj, char *_key);
-int   br_json_get_integer (json_object *obj, char *_key);
-bool  br_json_get_boolean (json_object *obj, char *_key);
+char* br_json_query_string  (json_object *obj, char *_key);
+int   br_json_query_integer (json_object *obj, char *_key);
+int   br_json_query_boolean (json_object *obj, char *_key);
+
+/* Function for convert between obj and string */
+struct json_object* br_json_string_to_obj (const char *str);
+char* br_json_obj_to_string (json_object *obj);
+
+/* Function for insert object to object */
+void br_json_add_obj (struct json_object *dest_obj, const char *_key, struct json_object *src_obj);
+
+/* Function for query string in object (recursively) */
+char* br_json_query_nested_string (struct json_object *obj, char *_key);
+
+#endif /* JSON_C_H */
--- src/jsonC.c
+++ src/jsonC.c
@@ -1,3 +1,10 @@
+/*
+ *  File : jsonC.c
+ *  Description : Wrapper for json-C
+ *	Date : 2013/11/11
+ *  Author : lester_hu@bandrich
+ *  Copyright BandRich .Inc All rights reserved.
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
@@ -31,7 +38,7 @@ int br_json_to_file (char *filename, json_object *obj)
 	assert(filename);
 	
 	// 0 : pass , -1: fail
-	return json_object_to_file(filename, obj);
+	return json_object_to_file_ext(filename, obj, JSON_FORMAT);
 }
 
 /*****************************************/
@@ -114,10 +121,10 @@ void br_json_update_boolean (json_object *obj, char *_key, bool _value)
 /*****************************************/
 /* 	Function for get value from object 	 */
 /*****************************************/
-char* br_json_get_string (json_object *obj, char *_key)
+static char* br_json_get_string (json_object *obj, char *_key)
 {
 	assert(_key);
-	
+
 	bool exist=false;
 	char *data;
 	json_object_object_foreach(obj, key, val) {
@@ -132,7 +139,7 @@ char* br_json_get_string (json_object *obj, char *_key)
 	return (exist == true) ? data : NULL;
 }
 
-int br_json_get_integer (json_object *obj, char *_key)
+static int br_json_get_integer (json_object *obj, char *_key)
 {
 	assert(_key);
 
@@ -150,7 +157,7 @@ int br_json_get_integer (json_object *obj, char *_key)
 	return (exist == true) ? data : -1;
 }
 
-bool br_json_get_boolean (json_object *obj, char *_key)
+static bool br_json_get_boolean (json_object *obj, char *_key)
 {
 	assert(_key);
 	
@@ -168,37 +175,131 @@ bool br_json_get_boolean (json_object *obj, char *_key)
 	return (exist == true) ? data : false;
 }
 
-// main function only for test
-#ifdef _DEBUG
+/*****************************************************/
+/* 	Function for convert between string and object 	 */
+/*****************************************************/
+struct json_object* br_json_string_to_obj (const char *str)
+{
+	return json_tokener_parse(str);
+}
+
+char* br_json_obj_to_string (json_object *obj)
+{
+	return (char*)json_object_to_json_string_ext(obj , JSON_FORMAT);
+}
+
+/**************************************************/
+/* 	Function for insert object to another object  */
+/**************************************************/
+void br_json_add_obj (struct json_object *dest_obj, const char *_key, struct json_object *src_obj)
+{
+	json_object_object_add(dest_obj, _key, src_obj);
+}
+
+char* br_json_query_string (json_object *obj, char *_key)
+{
+	char *data = (char*) br_json_get_string(obj, _key);
+	
+	// search first level
+	if (data == NULL) {
+		json_object_object_foreach(obj, key, val) {
+			if (json_object_get_type(val) == json_type_object) {
+				// Get sub object for parsing
+				struct json_object *sub_obj = json_object_object_get(obj, key);
+				data = br_json_query_string(sub_obj, _key);
+				if (data != NULL) {
+					return data;
+				}
+			}
+		}
+	} else {
+		return data;
+	}
+}
+
+int br_json_query_integer (json_object *obj, char *_key)
+{
+	int data = br_json_get_integer(obj, _key);
+	
+	// search first level
+	if (data == -1) {
+		json_object_object_foreach(obj, key, val) {
+			if (json_object_get_type(val) == json_type_object) {
+				// Get sub object for parsing
+				struct json_object *sub_obj = json_object_object_get(obj, key);
+				data = br_json_query_integer(sub_obj, _key);
+				if (data != -1) {
+					return data;
+				}
+			}
+		}
+	} else {
+		return data;
+	}
+}
+
+int br_json_query_boolean (json_object *obj, char *_key)
+{
+	bool data = br_json_get_boolean(obj, _key);
+	
+	// search first level
+	if (data != true) {
+		json_object_object_foreach(obj, key, val) {
+			if (json_object_get_type(val) == json_type_object) {
+				// Get sub object for parsing
+				struct json_object *sub_obj = json_object_object_get(obj, key);
+				data = br_json_query_boolean(sub_obj, _key);
+				if (data != false) {
+					return data;
+				}
+			}
+		}
+	} else {
+		return data;
+	}
+}
+
 void main(void)
 {
-	json_object *obj = br_json_load_from_file ("./test.json");
+	struct json_object* obj = br_json_load_from_file ("./test.json");
+	//DEBUG("[ORIGINAL] : %s\n", json_object_to_json_string(obj));
 
-	// add string to object
-	br_json_add_string(obj, "string", "hahahaha");
+	DEBUG("company : %s\n", 	(char*) br_json_query_string(obj, "company"));
+	DEBUG("title : %s\n", 		(char*) br_json_query_string(obj, "title"));
+	DEBUG("ID : %s\n", 			(char*) br_json_query_string(obj, "ID"));
+	DEBUG("SortAs : %s\n", 		(char*) br_json_query_string(obj, "SortAs"));
+	DEBUG("GlossTerm : %s\n", 	(char*) br_json_query_string(obj, "GlossTerm"));
+	DEBUG("Acronym : %s\n", 	(char*) br_json_query_string(obj, "Acronym"));
+	DEBUG("Abbrev : %s\n", 		(char*) br_json_query_string(obj, "Abbrev"));
+	DEBUG("para : %s\n", 		(char*) br_json_query_string(obj, "para"));
+	DEBUG("GML : %s\n", 		(char*) br_json_query_string(obj, "GML"));
+	DEBUG("GlossSee : %s\n", 	(char*) br_json_query_string(obj, "GlossSee"));
+	
+	// Failure case : Item "123" doesn't appear in test.json, it will return NULL.
+	DEBUG("123 : %s\n", 		(char*) br_json_query_string(obj, "123"));
+
+    // add string to object
+    br_json_add_string(obj, "key-br_json_add_string", "value-br_json_add_string");
 
 	// add integer to object
-	br_json_add_integer(obj, "integer", 19);
+	br_json_add_integer(obj, "key-br_json_add_integer", 19);
 
-	// add boolean to object
-	br_json_add_boolean(obj, "boolean", true);
+    // add boolean to object
+    br_json_add_boolean(obj, "key-br_json_add_boolean", true);
+      
+    // delete boolean from object (string, integer, and boolean are applicable)
+    br_json_del_object(obj, "company");
+
+    // Get integer value from object by key
+    int iResult = br_json_query_integer(obj, "integer");
+
+    // Get boolean value from object by key
+	bool bRet = br_json_query_boolean(obj, "boolean");
+
+    //DEBUG("%s\n", json_object_to_json_string(obj));
+
+    // Write obj to file
+    br_json_to_file("./dup.json", obj);
 	
-	// delete boolean from object (string, integer, and boolean are applicable)
-	br_json_del_object(obj, "purpose");
-
-	// Get string value from object by key
-	char *szResult = br_json_get_string(obj, "purpose");
-
-	// Get integer value from object by key
-	int iResult = br_json_get_integer(obj, "integer");
-
-	// Get boolean value from object by key
-	bool bRet = br_json_get_boolean(obj, "boolean");
-
-	DEBUG("%s\n", json_object_to_json_string(obj));
-
-	// Get boolean value from object by key
-	br_json_to_file("./dup.json", obj);
 	return;
 }
-#endif
