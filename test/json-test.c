626351bcc2f1ac434e7b1af952f9da0fb59264a9
lester_hu <jwhu1024@gmail.com>
2016-08-10 09:52:00 +0800

    dummy tester

--- test/json-test.c
+++ test/json-test.c
@@ -1,7 +1,22 @@
 #include "jsonC.h"
 
-void main (void)
+int iter_obj (json_object *obj)
 {
-    printf ("123\n");
-    return;
+    struct json_object_iterator it;
+    struct json_object_iterator itEnd;
+
+    it = json_object_iter_begin(obj);
+    itEnd = json_object_iter_end(obj);
+
+    while (!json_object_iter_equal(&it, &itEnd)) {
+        printf("%s\n", json_object_iter_peek_name(&it));
+        json_object_iter_next(&it);
+    }
+}
+
+int main (void)
+{
+    json_object *jobj = br_json_load_from_file ("../test.json");
+    iter_obj (jobj);
+    return 0;
 }
